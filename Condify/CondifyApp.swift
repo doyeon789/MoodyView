37787545f748d5c75ddc9fb09725b5680be8a23c
doyeon789 <ldyeon789@naver.com>
2025-05-14 21:09:25 +0900

    Initial Commit

---
+++ Condify/CondifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CondifyApp.swift
+//  Condify
+//
+//  Created by 도연 on 5/14/25.
+//
+
+import SwiftUI
+
+@main
+struct CondifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CondifyTests/CondifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  CondifyTests.swift
+//  CondifyTests
+//
+//  Created by 도연 on 5/14/25.
+//
+
+import Testing
+@testable import Condify
+
+struct CondifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
