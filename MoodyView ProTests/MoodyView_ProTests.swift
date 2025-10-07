bf56d31f85e8c9fc54b406fdbcbcb26b7ce6a9cc
doyeon789 <ldyeon789@naver.com>
2025-10-07 23:56:24 +0900

    Initial Commit

---
+++ MoodyView ProTests/MoodyView_ProTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoodyView_ProTests.swift
+//  MoodyView ProTests
+//
+//  Created by 도연 on 10/7/25.
+//
+
+import Testing
+@testable import MoodyView_Pro
+
+struct MoodyView_ProTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ MoodyView ProUITests/MoodyView_ProUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  MoodyView_ProUITestsLaunchTests.swift
+//  MoodyView ProUITests
+//
+//  Created by 도연 on 10/7/25.
+//
+
+import XCTest
+
+final class MoodyView_ProUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
